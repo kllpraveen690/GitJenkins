a24834e04e6e98b96badd886255a53662fa9c19e
kllpraveen690 <kllpraveen690@gmail.com>
2019-05-31 21:11:09 +0300

    Modified

--- HelloWorldFreeStyle.java
+++ HelloWorldFreeStyle.java
@@ -1,5 +1,6 @@
 public class HelloWorldFreeStyle{
  public static void main(String args[]){
      System.out.println("Hello World - Free Style- Git SCM Changes");
+	 System.out.println("Modify");
  }
 }
